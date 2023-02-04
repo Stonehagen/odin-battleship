1b95a5577910051c956b36bd24ed755e595f6465
Stonehagen <steinhagen@mailbox.org>
2023-02-04 23:08:43 +0100

    rewrite test again for Ship

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,7 +1,7 @@
 import { Ship } from "../src/Ship";
 
 describe('Ship Class', () => {
-  test('new Ship returns a new Object', () => {
+  it('returns a new Ship after calling with new', () => {
     const ship = new Ship(2);
     expect(ship).toBeInstanceOf(Ship);
     expect(ship.length).toBe(2);
