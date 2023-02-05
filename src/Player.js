5a5c317ae047f0860429e0e5e1fcb1e3633d6bf1
Stonehagen <steinhagen@mailbox.org>
2023-02-05 23:48:48 +0100

    add player attack method

--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,25 @@
+import { Gameboard } from './Gameboard';
+
 /* eslint-disable import/prefer-default-export */
 export const Player = (name) => {
   const player = {};
   player.name = name;
+  player.computer = false;
+  player.gameboard = Gameboard();
+  player.attack = (opponent, coords = null) => {
+    if (player.computer === true) {
+      let hitOrMiss = false;
+      while (!hitOrMiss) {
+        const compCoords = [
+          Math.floor(Math.random() * 10) + 1,
+          Math.floor(Math.random() * 10) + 1,
+        ];
+        hitOrMiss = opponent.gameboard.receiveAttack(compCoords);
+      }
+      return true;
+    }
+    return opponent.gameboard.receiveAttack(coords);
+  };
 
   return player;
 };
